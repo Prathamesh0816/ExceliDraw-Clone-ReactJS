4ec8d73c8e6d849df12708dd59081cb1230416d1
Prathamesh Katikar <pgkatikar@gmail.com>
2025-01-30 17:29:10 +0530

    added ondrag draw

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,92 +1,186 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Stage, Layer, Rect, Circle, TextPath, Text } from "react-konva";
+import { Stage, Layer, Rect, Circle, TextPath, Text, Line } from "react-konva";
+
 const Home = () => {
   const [shapes, setShapes] = useState([{}]);
+
+  const [shapeType, setShapeType] = useState("rect"); // circle
   const canvasRef = useRef(null);
+
+  let idRef = useRef(null);
+
   useEffect(() => {
     if (canvasRef.current) {
       let canvasEl = canvasRef.current;
       let x = 0;
       let y = 0;
-      let height = 0;
-      let width = 0;
-      canvasEl.addEventListener("mousedown", (e) => {
+      let isDrawing = false;
+
+      const handleMouseDown = (e) => {
+        isDrawing = true;
         x = e.clientX;
         y = e.clientY;
-      });
-      canvasEl.addEventListener("mouseup", (e) => {
-        width = e.clientX - x;
-        height = e.clientY - y;
-        console.log("x is ", x, "y is ", y);
-        console.log("height is ", height, "widht is ", width);
+
+        idRef.current = Date.now();
+
+        console.log(shapeType);
+
         let shape = {
+          points: [x, y],
           x: x,
           y: y,
-          width: width,
-          height: height,
+          width: 0,
+          height: 0,
+          radius: 0,
+          type: shapeType,
           fill: "red",
-          type: "rect",
-          id: Date.now(),
+          stroke: "white",
+          strokeWidth: 30,
+          opacity: 0.5,
+          id: idRef.current,
         };
+
         setShapes((p) => [...p, shape]);
-      });
+      };
+
+      const handleMouseMove = (e) => {
+        if (!isDrawing) return;
+
+        setShapes((shapes) => {
+          return shapes.map((shape) => {
+            if (shape.type === "rect" && shape.id === idRef.current) {
+              shape.height = e.clientY - y;
+              shape.width = e.clientX - x;
+            } else if (shape.type === "circle" && shape.id === idRef.current) {
+              let d = (e.clientX - x) ** 2 + (e.clientY - y) ** 2;
+              let r = Math.sqrt(d);
+              shape.radius = r;
+            } else if (shape.type === "line" && shape.id === idRef.current) {
+              shape.points = [...shape.points, e.clientX, e.clientY];
+            }
+            return shape;
+          });
+        });
+      };
+
+      const handleMouseUp = (e) => {
+        isDrawing = false;
+      };
+
+      canvasEl.addEventListener("mousedown", handleMouseDown);
+
+      canvasEl.addEventListener("mousemove", handleMouseMove);
+
+      canvasEl.addEventListener("mouseup", handleMouseUp);
+
       return () => {
-        canvasEl.removeEventListener("mouseup");
-        canvasEl.removeEventListener("mousedown");
-        canvasEl.removeEventListener("mousemove");
+        canvasEl.removeEventListener("mouseup", handleMouseUp);
+        canvasEl.removeEventListener("mousedown", handleMouseDown);
+        canvasEl.removeEventListener("mousemove", handleMouseMove);
       };
     }
-  }, []);
-  function drawRect() {
-    let shape = {
-      x: 200,
-      y: 200,
-      width: 200,
-      height: 200,
-      fill: "red",
-      type: "rect",
-      id: Date.now(),
-    };
-    setShapes((p) => [...p, shape]);
+  }, [shapeType]);
+
+  function handleDragEnd(e, id) {
+    console.log(e.evt);
+
+    setShapes((shapes) => {
+      return shapes.map((shape) => {
+        if (shape.id === id) {
+          // not correct
+          shape.x = e.evt.clientX;
+          shape.y = e.evt.clientY;
+        }
+        return shape;
+      });
+    });
   }
+
   return (
     <>
-      {shapes.length === 0 ? (
-        <div className="bg-slate-800 min-h-screen text-slate-100">
-          <p className="text-center text-4xl font-bold">
-            Welcome to Excellidraw
-          </p>
-          <p className="text-center">Worlds #1 note making/drawing app</p>
-        </div>
-      ) : (
-        <>
-          <button onClick={drawRect}>Add react</button>
-          <Stage
-            className="bg-slate-800 min-h-screen text-slate-100"
-            width={window.innerWidth}
-            height={window.innerHeight}
-            ref={canvasRef}
+      <div className="fixed z-40 top-4 flex justify-center items-center w-full">
+        <div className=" text-white flex gap-4 p-2 bg-slate-500 rounded-2xl">
+          {/* todo improve this : - create array for shapes and
+           use map on that array to return all these buttons */}
+          <button
+            onClick={() => setShapeType("rect")}
+            className="border-2 border-amber-100
+             px-4 py-2 rounded-2xl cursor-pointer"
           >
-            <Layer>
-              {shapes.map((shape) => {
-                if (shape.type === "rect") {
-                  return (
-                    <Rect
-                      key={shape.id}
-                      x={shape.x}
-                      y={shape.y}
-                      width={shape.width}
-                      height={shape.height}
-                      fill={shape.fill}
-                    />
-                  );
-                }
-              })}
-            </Layer>
-          </Stage>
-        </>
-      )}
+            Rectangle
+          </button>
+          <button
+            onClick={() => setShapeType("circle")}
+            className="border-2 border-amber-100
+             px-4 py-2 rounded-2xl cursor-pointer"
+          >
+            Circle
+          </button>
+          <button
+            onClick={() => setShapeType("line")}
+            className="border-2 border-amber-100
+             px-4 py-2 rounded-2xl cursor-pointer"
+          >
+            Draw
+          </button>
+        </div>
+      </div>
+
+      <Stage
+        className="bg-zinc-950 min-h-screen text-slate-100"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={canvasRef}
+      >
+        <Layer>
+          {shapes.map((shape) => {
+            if (shape.type === "rect") {
+              return (
+                <Rect
+                  key={shape.id}
+                  x={shape.x}
+                  y={shape.y}
+                  width={shape.width}
+                  height={shape.height}
+                  fill={shape.fill}
+                  draggable
+                  strokeWidth={5}
+                  opacity={0.8}
+                  stroke={"white"}
+                  onDragEnd={(e) => handleDragEnd(e, shape.id)}
+                />
+              );
+            } else if (shape.type === "circle") {
+              return (
+                <Circle
+                  x={shape.x}
+                  y={shape.y}
+                  radius={shape.radius}
+                  fill={"red"}
+                  draggable
+                  strokeWidth={5}
+                  opacity={0.8}
+                  stroke={"white"}
+                />
+              );
+            } else if (shape.type === "line") {
+              return (
+                <Line
+                  points={shape.points}
+                  stroke={"green"}
+                  strokeWidth={2}
+                  lineJoin="round"
+                  // dash={[33, 10]}
+                  lineCap="round"
+                  tension={0.5}
+                />
+              );
+            }
+          })}
+        </Layer>
+      </Stage>
     </>
   );
 };
+
 export default Home;
